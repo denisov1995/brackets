6ea81a5470f603d4538622e877e299936fec7f4f
denisov1995 <bysama1995@gmail.com>
2021-03-11 06:16:31 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = bracketsConfig.map(i => i.join(''))
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+    for (let j = 0; j < array.length; j++) {
+      str = str.replace(array[j], '')
+    }
+  }
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
